a3f0689bc5a9b6c2b5ecdb87e03395efad0d1a0b
curtisgiddings <curtisgiddings@gmail.com>
2013-03-05 00:57:08 -0500

    Initial RoR setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweetcritic::Application.config.secret_token = 'cff79f1cd02e4cb7ff7bdf5aacb1155bd951ed65edb6c272a4d691d34cd25abc852a3e13150e897368ba9863bfc2de368013fb141a61ac24b5adf1a73ea14785'
