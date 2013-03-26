fb383c88744be0b5a0820257c7ab11f2f7767548
curtisgiddings <curtisgiddings@gmail.com>
2013-03-26 01:43:28 -0400

    Added interface with TheMovieDB.org

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ group :development, :test do
   gem 'sqlite3', '1.3.5'
   gem 'rspec-rails', '2.11.0'
   gem 'twitter'
+  gem 'ruby-tmdb3'
 end
 
 
--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,7 @@
 class SearchController < ApplicationController
   def index
+  	@movie = TmdbMovie.find(:title => params[:search], :limit => 1)
+  	@cast = Tmdb.api_call('movie/casts', {:id => @movie.id}, "en")
+  	@images = Tmdb.api_call("movie/images", {:id => @movie.id}, "en")
   end
 end
