683147160916d2ae5dc0d1dc2e15e135cebd0288
curtisgiddings <curtisgiddings@gmail.com>
2013-03-27 03:00:39 -0400

    Added formatting updates

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,9 +1,9 @@
 module SearchHelper
 	def tweetsearch(params)
 	  if params[:search].include? 'movie'
-		    @tweets = Twitter.search( '"' + params[:search] + '"', :count => 15)
+		 Twitter.search( '"' + params[:search] + '"', :count => 15)
   	  else
-  	  	 @tweets = Twitter.search('"' + params[:search] + '"' + 'movie', :count => 15)
+  	  	 Twitter.search('"' + params[:search] + '"' + 'movie', :count => 15)
   	  end
   	end
 end
