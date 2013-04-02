b379d833c5bbbb46b1f927abc0790125d90c1011
curtisgiddings <curtisgiddings@gmail.com>
2013-04-01 22:36:11 -0400

    Testing (commits not appearing?)

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,13 +5,13 @@ set :repository,  "git@github.com:curtisgiddings/140critic.git"
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 set :scm, "git"
-set :user, "deployer"  # The server's user for deploys
-set :scm_passphrase, "p@ssw0rd"  # The deploy user's password
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
-
+set :user, "curtisgiddings"  # The server's user for deploys
+set :scm_passphrase, "#kwwoxn13#"  # The deploy user's password
+role :web, "199.231.86.177"                          # Your HTTP server, Apache/etc
+role :app, "199.231.86.177"                          # This may be the same as your `Web` server
+role :db,  "199.231.86.177", :primary => true 
+set :use_sudo, false 
+# This is where Rails migrations will run
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
 
